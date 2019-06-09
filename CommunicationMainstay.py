ca7eafbf0fd2495013a2eddbf264248742c45235
Polani Balaji <40557109+polanibalaji1@users.noreply.github.com>
2019-06-09 21:03:18 +0530

    Update CommunicationMainstay.py

--- CommunicationMainstay.py
+++ CommunicationMainstay.py
@@ -1,6 +1,6 @@
 import socket
 s=socket.socket()
-s.bind(('192.168.43.160',12345))
+s.bind(('192.168.43.160',12345))    #Use the IP address of your system after connecting to WIFI / Hotspot
 s.listen(5)
 while(true):
     c,addr=s.accept()
